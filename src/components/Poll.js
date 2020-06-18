50dbe213afa9ffdb0961ab025d14a658ea92bc29
mark-quinn <markquinn97@hotmail.com>
2020-06-18 11:33:36 +0100

    add general layout of poll cards

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Poll extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Redirect to poll/:id');
+  }
+
   render() {
     const { question, author } = this.props;
     return (
-      <div className="card w-50">
+      <div className="card">
         <div className="card-header bg-light">
           <p className="font-weight-bold">{author.name} asks:</p>
         </div>
         <div className="card-body">
-          <p>Question info here</p>
+          <p className="font-weight-bold">Would you rather</p>
+          <p>...{question.optionOne.text.slice(0, 14)}...</p>
+          <button
+            className="btn btn-block btn-outline-success"
+            onClick={this.handleSubmit}
+          >
+            View Poll
+          </button>
         </div>
       </div>
     );

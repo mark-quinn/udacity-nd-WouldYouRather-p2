1b6a01eb62a2721ca1751e942567486510f39d5b
mark-quinn <markquinn97@hotmail.com>
2020-06-18 01:01:43 +0100

    handle unanswered/answered radio btn logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Home extends Component {
+  state = {
+    questionSelected: "unanswered",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      questionSelected: e.target.value,
+    });
+  };
+
   render() {
+    const { questionSelected } = this.state;
     const { users, questions, authedUser } = this.props;
     const user = users[authedUser];
     const allQuestions = Object.keys(questions);
@@ -12,7 +23,37 @@ class Home extends Component {
       .filter((x) => !allQuestions.includes(x))
       .concat(allQuestions.filter((x) => !answeredQuestions.includes(x)));
 
-    return <p>Home Component</p>;
+    return (
+      <div className="container d-flex justify-content-center mt-2">
+        <div className="card w-50">
+          <div className="card-header bg-light">
+            <div className="font-weight-bold mb-0">
+              <form className="d-flex justify-content-around">
+                <input
+                  type="radio"
+                  name="questions"
+                  value="unanswered"
+                  checked={questionSelected === "unanswered"}
+                  onChange={this.handleChange}
+                />
+                Unanswered
+                <input
+                  type="radio"
+                  name="questions"
+                  value="answered"
+                  checked={questionSelected === "answered"}
+                  onChange={this.handleChange}
+                />
+                Answered
+              </form>
+            </div>
+          </div>
+          <div className="card-body">
+            <p>Display list of questions</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

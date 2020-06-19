28f30ece4d2f958f0cd83b8d1ba955d38c49533b
mark-quinn <markquinn97@hotmail.com>
2020-06-19 14:17:18 +0100

    add create new question page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from "../actions/shared";
 import SignIn from "./SignIn";
 import Home from './Home';
 import PollPage from './PollPage';
+import NewQuestion from './NewQuestion';
 
 class App extends Component {
   componentDidMount() {
@@ -15,7 +16,7 @@ class App extends Component {
       <div>
         {this.props.loading === true ? null : (
           <div>
-            <PollPage id={"8xf0y6ziyjabvozdd253nd"} />
+            <NewQuestion />
           </div>
         )}
       </div>
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class NewQuestion extends Component {
+  state = {
+    optionOne: "",
+    optionTwo: "",
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { optionOne, optionTwo } = this.state;
+
+    if (optionOne && optionTwo) {
+      // TODO: dispatch new question event
+    }
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({
+      ...this.state,
+      [e.target.name]: value,
+    });
+  };
+
+  render() {
+    const { optionOne, optionTwo } = this.state;
+
+    return (
+      <div className="container d-flex justify-content-center mt-2">
+        <div className="card w-50">
+          <div className="card-header bg-light">
+            <h3 className="font-weight-bold mb-0 text-center">
+              Create New Question
+            </h3>
+          </div>
+          <div className="card-body">
+            <p>Complete the question:</p>
+            <p className="font-weight-bold">Would you rather ...</p>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                className="w-100"
+                name="optionOne"
+                value={optionOne}
+                placeholder="Enter Option One Text Here"
+                onChange={this.handleChange}
+              />
+              <p className="font-weight-bold text-center">OR</p>
+              <input
+                className="w-100"
+                name="optionTwo"
+                value={optionTwo}
+                placeholder="Enter Option Two Text Here"
+                onChange={this.handleChange}
+              />
+              <button className="btn btn-success btn-block mt-3">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser,
+  };
+}
+
+export default connect(mapStateToProps)(NewQuestion);

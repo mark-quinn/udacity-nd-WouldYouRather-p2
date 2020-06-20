ac8ac8bcd5caee36a6269699a90ae7617f01a658
mark-quinn <markquinn97@hotmail.com>
2020-06-20 10:46:54 +0100

    handle user logout action

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,49 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import { removeAuthedUser } from "../actions/authedUser";
+import { withRouter } from "react-router-dom";
 
-const Nav = (props) => {
-  const { user } = props;
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink to="/" exact activeClassName="active" className="nav-link">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/new"
-              exact
-              activeClassName="active"
-              className="nav-link"
-            >
-              New Question
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/leaderboard"
-              exact
-              activeClassName="active"
-              className="nav-link"
-            >
-              Leader Board
-            </NavLink>
-          </li>
-        </ul>
-        {user && (
-          <div>
-            <span className="navbar-text">{`Hello, ${user.name}`}</span>
-            <button className="ml-2 btn btn-link">Logout</button>
-          </div>
-        )}
-      </div>
-    </nav>
-  );
-};
+class Nav extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.dispatch(removeAuthedUser(this.props.user));
+    this.props.history.push('/sign-in');
+  };
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/"
+                exact
+                activeClassName="active"
+                className="nav-link"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/new"
+                exact
+                activeClassName="active"
+                className="nav-link"
+              >
+                New Question
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/leaderboard"
+                exact
+                activeClassName="active"
+                className="nav-link"
+              >
+                Leader Board
+              </NavLink>
+            </li>
+          </ul>
+          {user && (
+            <div>
+              <span className="navbar-text">{`Hello, ${user.name}`}</span>
+              <button className="ml-2 btn btn-link" onClick={this.handleSubmit}>
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      </nav>
+    );
+  }
+}
 
 function mapStateToProps({ authedUser, users }) {
   let user = null;
@@ -56,4 +74,4 @@ function mapStateToProps({ authedUser, users }) {
   };
 }
 
-export default connect(mapStateToProps)(Nav);
+export default withRouter(connect(mapStateToProps)(Nav));

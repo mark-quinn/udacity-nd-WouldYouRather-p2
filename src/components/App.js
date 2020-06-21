3f4ed2e4c91e7e2c234efc59d5a067ab24963c6e
mark-quinn <markquinn97@hotmail.com>
2020-06-22 00:05:39 +0100

    chore: remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,9 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
-import SignIn from "./SignIn";
-import Home from "./Home";
-import PollPage from "./PollPage";
-import NewQuestion from "./NewQuestion";
-import Leaderboard from "./Leaderboard";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import LoadingBar from "react-redux-loading";
 import Nav from "./Nav";
-import NotFound from "./NotFound";
-import ProtectedRoute from "./ProtectedRoute";
 import Routes from "./Routes";
 
 class App extends Component {
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import SignIn from "./SignIn";
 import Home from "./Home";
 import PollPage from "./PollPage";
